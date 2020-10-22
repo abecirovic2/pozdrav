ba17c975a8c9ce2d9edbefd62e1953b507c2e0bf
Almir Becirovic <almir__b@outlook.com>
2020-10-22 17:24:36 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/predavanje02/Main.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.predavanje02;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world");
+    }
+}
